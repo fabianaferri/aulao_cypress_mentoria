fc29b52156f1a8fe0fa63c47434278cf8c11971e
fabianaferri <fabianaferri@gmail.com>
2024-09-04 10:25:35 -0300

    inicializando o repositosito com o arquivo do aulao de cypress-mentoria julio de lima

---
+++ 3-workshop.cy.js
@@ -0,0 +1,28 @@
+
+describe('financas', () =>{
+
+    //cada it é um test
+
+    it('cadastrar uma nova entrada' , () =>{
+        cy.visit('https://devfinance-agilizei.netlify.app/')
+
+        cy.contains('Nova Transação').click()
+        cy.get('#description').type('mercadinho')
+        cy.get('#amount').type('12.50')
+        cy.get('#date').type('2024-09-02')
+        cy.get('button').click()
+
+        cy.contains('Nova Transação').click()
+        cy.get('#description').type('mercadinho2')
+        cy.get('#amount').type('12.50')
+        cy.get('#date').type('2024-09-02')
+        cy.get('button').click()
+
+        //should
+        cy.get('table tbody tr').should('have.length',2)
+    })
+
+
+
+    
+});
